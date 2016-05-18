3a1a525254470ca5e55c86c2cfb54c8136d9ec48
qiaoshi123 <2628868722@qq.com>
2016-05-18 12:49:05 +0800

    个人简历

---
+++ js/jianli.js
@@ -0,0 +1,79 @@
+/**
+ * Created by qiaoshi on 2016/5/15.
+ */
+$(function(){
+    $(document).on('touchmove',function(e){
+        e.preventDefault();
+    });
+    var winH=document.documentElement.clientHeight,winW=document.documentElement.clientWidth;
+    var desH=1008,desW=640;
+    if(winW/winH<=desW/desH){
+        var scale=winH/desH;
+        $('.main').css('transform','scale('+scale+')')
+    }else {
+        scale = winW / desW;
+        $('.main').css('transform','scale('+scale+')')
+    }
+    var $oLis=$('.list>li');
+    var Index=null,prevIndex=0,step=1/ 4,startY=null, movePos=null;
+        slideFn();
+        function slideFn(){
+        $oLis.on('touchstart',function(e){
+        Index=$(this).index();
+        startY= e.originalEvent.changedTouches[0].pageY;
+        $(this).on('touchmove ',function(e){
+            $(this).attr('flag','true');
+            $(this).siblings().hide();
+            $oLis.removeClass('zIndex');
+             var curY=e.originalEvent.changedTouches[0].pageY;
+             movePos=curY-startY;
+            if(movePos<0){
+                prevIndex=Index==$oLis.length-1?0:Index+1;
+                $(this).css('transform','translate(0,'+movePos/2+'px)scale('+(1-Math.abs(movePos/winH*step))+') ');
+                $oLis.eq(prevIndex).css('transform','translate(0,'+(winH+movePos)+'px) ');
+            }else if(movePos>0){
+                prevIndex=Index==0?$oLis.length-1:Index-1;
+                $oLis.eq(prevIndex).css('transform','translate(0,'+(-winH+movePos)+'px)');
+                $(this).css('transform','translate(0,'+movePos/2+'px) scale('+(1-Math.abs(movePos/winH*step))+')');
+            }else{
+                $(this).attr('flag','false');
+            }
+            $oLis.eq(prevIndex).show().addClass('zIndex');
+        });
+        $(this).on('touchend',function(e){
+            if($(this).attr('flag')=='true') {
+                if(movePos<0) {//up
+                    $(this).css('transform','translate(0,'+(-winH*step*2)+'px) scale('+(1-step)+') ');
+                }else if(movePos>0) {//down
+                    $(this).css('transform','translate(0,'+(winH*step*2)+'px) scale('+(1-step)+') ')
+                }
+                $oLis.eq(prevIndex).css('transform', 'translate(0,0)');
+                $oLis.eq(prevIndex).css('webkitTransition', '0.7s');
+                $(this).css('webkitTransition', '0.7s');
+                $(this).off('touchmove');
+                $(this).off('touchend');
+                $(this).attr('flag','false');
+             }
+            })
+        });
+    }
+        $oLis.on('webkitTransitionEnd',function(e){
+            if(!$oLis.is(e.target))return;
+            $(this).css('transform','');
+            $(this).css('transition','');
+            $oLis.eq(prevIndex).css('transition','');
+            $oLis.eq(Index).hide();
+        });
+    var music=document.getElementById('musicMp3');
+    music.play();
+    music.flag=true;
+    $('#musicBtn').on('tap',function(){
+        if(music.flag){
+            music.pause();
+            music.flag=false;
+        }else{
+            music.play();
+            music.flag=true;
+        }
+    });
+});
